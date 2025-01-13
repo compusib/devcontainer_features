eeafc2143fd320f09ad92ec8899c4d2fc46cf8ba
Paul Balomiri <paulbalomiri@gmail.com>
2025-01-13 19:12:53 +0000

    fix(bashrc): duplicate run test

--- src/bashrc/install.sh
+++ src/bashrc/install.sh
@@ -2,9 +2,10 @@
 RUNFILE="$_REMOTE_USER_HOME/.devcontainer/bashrcFeatureInstallCounter"
 
 if [ -e $RUNFILE ]; then
-    echo "RUNFILE exists"
+  echo "RUNFILE exists"
 else
-   . ./do_install.sh
+  touch  $RUNFILE
+  . ./do_install.sh
 fi
 
 # function run_count {
